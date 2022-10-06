3b04f94ea92fcb10d5b30bed9213043104118f7a
postl <postl@LAPTOP-2A544SQ1>
2022-10-06 13:08:54 +0200

    Exceptinon_city_not_found

--- src/open_weather.py
+++ src/open_weather.py
@@ -8,7 +8,7 @@ from BaseWeatherAPI import BaseClassAPI
 class OpenWeatherAPI(BaseClassAPI):
     API_KEY = "dda8da3e457ddaa3f2db53e90a042e88"
 
-    request_url = f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},at&appid={API_KEY}&lang=de"
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?zip=8230,at&appid={API_KEY}&lang=de"
     data = requests.get(request_url).json()
     response = requests.get(request_url)
         
